5706cfb5ff14afe148decc77582e6ebf20f7db4d
Arvind_Kokane <kokanearvind875@gmail.com>
2026-01-07 00:18:20 +0530

    Clean up imports in app.ts
    Removed unused imports from the App component.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './login/login';
-import { NgClass } from '@angular/common';
-import { Ngclass } from './ngclass/ngclass';
+
 import { Registrationservice } from './registrationservice';
-import { Register } from './register/register';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Login,NgClass,Ngclass,Register],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
